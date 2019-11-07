a61f07b79b4dfa6e2a1d6885e8b388ce12856937
kranach2 <kis_79rc@yahoo.com>
2019-11-07 16:18:39 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import styles from "./css/App.module.css";
+
+function App() {
+  const [day, setDay] = useState("00");
+  const [hour, setHour] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [second, setSecond] = useState("00");
+  const [message, setMessage] = useState("");
+  const targetDate = new Date("Nov 14, 2019 15:55:25").getTime();
+  const countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const timeLeft = targetDate - now;
+    setDay(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+    setHour(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    setMinute(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
+    setSecond(Math.floor((timeLeft % (1000 * 60)) / 1000));
+    if (timeLeft < 0) {
+      clearInterval(countdown);
+      setDay("00");
+      setHour("00");
+      setMinute("00");
+      setSecond("00");
+      setMessage("Welcome aboard");
+    }
+  }, 1000);
+
+  return (
+    <div className="App">
+      <header className="App-header"> </header>
+      <h1>Coming soon!!!</h1>
+      <div className={styles.time}>
+        <div className={styles.group}>
+          <span className={styles.day}>{day}</span>
+          &nbsp;{" "}
+          <span style={{ fontWeight: "bold", fontSize: "20px" }}>day</span>{" "}
+          &nbsp;
+          <span className={styles.hour}>{hour}</span> &nbsp;
+          <span style={{ fontWeight: "bold", fontSize: "20px" }}>hour</span>{" "}
+          &nbsp;
+          <span className={styles.minute}>{minute}</span> &nbsp;{" "}
+          <span style={{ fontWeight: "bold", fontSize: "20px" }}>minutes</span>{" "}
+          &nbsp;
+          <span className={styles.second}>{second}</span>&nbsp;{" "}
+          <span style={{ fontWeight: "bold", fontSize: "20px" }}>second</span>
+        </div>
+      </div>
+      <h1> {message} </h1>{" "}
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+//const usersRouter = require("./routes/users");
+//const path = require("path");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+//app.use("/users", usersRouter);
+
+
+// const uri = process.env.ATLAS_URI;
+// mongoose
+//   .connect(uri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("Db connected");
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("Mongodb database is connected");
+// });
+
+ if(process.env.NODE_ENV === "production"){
+   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  
+ });
+ }
+
+
+
+app.listen(port, () => {
+  console.log(`The server is running in port ${port}`);
+});
